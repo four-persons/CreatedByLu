d0de0ce6113b2aed03b29b04b33de384dc7bb7a7
qingqingFu <fuyuanqing19970616@gmail.com>
2019-04-10 22:27:28 +0200

    commit

--- src/githubfourpeople/GithubFourPeople.java
+++ src/githubfourpeople/GithubFourPeople.java
@@ -15,7 +15,7 @@ public class GithubFourPeople {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hello Guys, I changed again...");
+        System.out.println("Hello Guys, I changed one more time...");
 
 
 
