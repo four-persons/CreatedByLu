5c6d330262cf0d2c57d9f8c30c5709a224f804d2
qingqingFu <fuyuanqing19970616@gmail.com>
2019-04-10 22:48:18 +0200

    commit

--- src/githubfourpeople/GithubFourPeople.java
+++ src/githubfourpeople/GithubFourPeople.java
@@ -15,7 +15,7 @@ public class GithubFourPeople {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hello Guys, I changed one more time and this should be in branch one...");
+        System.out.println("Hello Guys, I changed and this should be in branch one...");
 
 
 
